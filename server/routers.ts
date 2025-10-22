49a780071668cd1233576276aa16afa5baeaf5d0
R-feitosa <empresas@roneelyfeitosa.com.br>
2025-10-22 09:05:31 -0400

    Revert "Feature: Reduzir jogo para 3 situações por fase (em vez de todas)"
    This reverts commit 6dac228baba26e444db3b40f933c50bf4f108b44.

--- server/routers.ts
+++ server/routers.ts
@@ -93,16 +93,12 @@ export const appRouter = router({
           throw new Error("Fase não encontrada");
         }
 
-        // Selecionar apenas 3 dilemas aleatórios da fase
-        const shuffledDilemas = [...faseData.dilemas].sort(() => Math.random() - 0.5);
-        const selectedDilemas = shuffledDilemas.slice(0, 3);
-
         return {
           fase: faseData.fase,
           setor: faseData.setor,
           supervisor: faseData.supervisor,
           cenario: faseData.cenario,
-          dilemas: selectedDilemas,
+          dilemas: faseData.dilemas,
           trainee: {
             name: trainee.name,
             currentPhase: trainee.currentPhase,
